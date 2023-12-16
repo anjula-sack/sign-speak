16575f39625e5455ab93d09b54ecc4d8326c9f4d
Bawantha <bawanthaprasad4@gmail.com>
2023-12-16 21:07:20 +0530

    implement voice recognition

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
 import os
 import tkinter as tk
+import speech_recognition as sr
 from PIL import Image, ImageTk
+
+recognizer = sr.Recognizer()
+
+#Speech recognition
+
+def capture_voice_input():
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = recognizer.listen(source)
+    return audio
+
+def convert_voice_to_text(audio):
+    try:
+        text = recognizer.recognize_google(audio)
+        print("You said: " + text)
+    except sr.UnknownValueError:
+        text = ""
+        print("Sorry, I didn't understand that.")
+    except sr.RequestError as e:
+        text = ""
+        print("Error; {0}".format(e))
+    return text
+
+def process_voice_command(text):
+    if "goodbye" in text.lower():
+        print("Command Recognized....")
+        print("Closing application....")
+        return True
+    
+
+
+    
 
 def text_to_asl(text):
     asl_mapping = {
@@ -43,12 +76,25 @@ def text_to_asl(text):
         panel.pack(side="left")
 
 if __name__ == "__main__":
-    input_text = input("Enter text to convert to ASL: ")
+    
+    end_program = False
+    while not end_program:
+        audio = capture_voice_input()
+        text = convert_voice_to_text(audio)
+        end_program = process_voice_command(text)
 
-    root = tk.Tk()
-    root.title("ASL Fingerspelling")
+        if text != "" and text != "goodbye" :
+            root = tk.Tk()
+            root.title("ASL Fingerspelling")
+            text_to_asl(text)
+            root.mainloop()
+        
+    
 
-    text_to_asl(input_text)
+    
+    
 
-    root.mainloop()
+    
+
+    
 
