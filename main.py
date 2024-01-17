6faad0b1725f9f185e54f0b8a32da23f01717b2d
Bawantha <bawanthaprasad4@gmail.com>
2024-01-17 14:30:51 +0530

    Add empty image

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import os
 import tkinter as tk
 import speech_recognition as sr
 from PIL import Image, ImageTk
+import threading
 
 recognizer = sr.Recognizer()
 
-#Speech recognition
+# Speech recognition
 
 def capture_voice_input():
     with sr.Microphone() as source:
@@ -17,23 +18,20 @@ def convert_voice_to_text(audio):
     try:
         text = recognizer.recognize_google(audio)
         print("You said: " + text)
+        return text
     except sr.UnknownValueError:
-        text = ""
         print("Sorry, I didn't understand that.")
+        return ""
     except sr.RequestError as e:
-        text = ""
         print("Error; {0}".format(e))
-    return text
+        return ""
 
 def process_voice_command(text):
     if "goodbye" in text.lower():
         print("Command Recognized....")
         print("Closing application....")
         return True
-    
-
-
-    
+    return False
 
 def text_to_asl(text):
     asl_mapping = {
@@ -75,26 +73,31 @@ def text_to_asl(text):
         panel.image = img
         panel.pack(side="left")
 
-if __name__ == "__main__":
-    
-    end_program = False
-    while not end_program:
+def start_stop_button():
+    global listening
+    listening = not listening
+    if listening:
+        start_stop_btn.config(text="Stop Listening")
+        threading.Thread(target=listen_and_process).start()
+    else:
+        start_stop_btn.config(text="Start Listening")
+
+def listen_and_process():
+    global end_program
+    while listening and not end_program:
         audio = capture_voice_input()
         text = convert_voice_to_text(audio)
         end_program = process_voice_command(text)
+        text_to_asl(text)
 
-        if text != "" and text != "goodbye" :
-            root = tk.Tk()
-            root.title("ASL Fingerspelling")
-            text_to_asl(text)
-            root.mainloop()
-        
-    
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("Speech Recognition")
 
-    
-    
+    listening = False
+    end_program = False
 
-    
+    start_stop_btn = tk.Button(root, text="Start Listening", command=start_stop_button)
+    start_stop_btn.pack()
 
-    
-
+    root.mainloop()
